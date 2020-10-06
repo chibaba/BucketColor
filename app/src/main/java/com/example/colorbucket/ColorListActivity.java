66463d78ee41b5b6133ddf4f7fdb450b0923c853
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-06 01:06:55 +0100

    first commit

---
+++ app/src/main/java/com/example/colorbucket/ColorListActivity.java
@@ -0,0 +1,14 @@
+package com.example.colorbucket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colorlist_main);
+    }
+}
