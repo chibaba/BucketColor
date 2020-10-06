39e58e81ed167d0009a9133ce1f8468ba7c5972d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-06 04:38:59 +0100

    wip

--- app/src/main/java/com/example/colorbucket/ColorListActivity.java
+++ app/src/main/java/com/example/colorbucket/ColorListActivity.java
@@ -1,14 +1,28 @@
 package com.example.colorbucket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class ColorListActivity extends AppCompatActivity {
 
+CardView  red, purple, maroon, royalBlue, accent, yellow, green, white, black, blue, orange, violet, pink, cyan, azure, primaryColor, brown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.colorlist_main);
+
+        renderView();
+        changeOnClick()
+    }
+
+    public void renderView() {
+        red = findViewById(R.id.Red);
+        purple = findViewById(R.id.Purple);
+        maroon = findViewById(R.id.Maroon);
+        royalBlue = findViewById(R.id.RoyalBlue);
+        accent = findViewById(R.id.Accent);
     }
 }
